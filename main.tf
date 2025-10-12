d9feb96db00028dfd4ba3498bee317860d9e97a7
Leonid Lachmann <148564629+leoleiden@users.noreply.github.com>
2025-10-12 19:38:20 +0300

    Update main.tf 2

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0.0"
+      version = "~>3.0"
     }
   }
 }
@@ -17,28 +17,31 @@ resource "azurerm_resource_group" "rg" {
 }
 
 module "network" {
-  source                      = "./modules/network"
+  source = "./modules/network"
+
   resource_group_name         = azurerm_resource_group.rg.name
   location                    = azurerm_resource_group.rg.location
   virtual_network_name        = var.virtual_network_name
-  vnet_address_prefix         = var.vnet_address_prefix
+  vnet_address_prefix         = [var.vnet_address_prefix]
   subnet_name                 = var.subnet_name
-  subnet_address_prefix       = var.subnet_address_prefix
+  subnet_address_prefix       = [var.subnet_address_prefix]
   network_security_group_name = var.network_security_group_name
   public_ip_address_name      = var.public_ip_address_name
   dns_label                   = var.dns_label
 }
 
 module "storage" {
-  source              = "./modules/storage"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  storage_account_name = var.storage_account_name_app_artifacts
-  storage_container_name = var.storage_container_name_app_artifacts
+  source = "./modules/storage"
+
+  resource_group_name    = azurerm_resource_group.rg.name
+  location               = azurerm_resource_group.rg.location
+  storage_account_name   = var.storage_account_name
+  storage_container_name = var.storage_container_name
 }
 
 module "compute" {
-  source              = "./modules/compute"
+  source = "./modules/compute"
+
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   vm_name             = var.vm_name
@@ -46,6 +49,6 @@ module "compute" {
   ssh_key_public      = var.ssh_key_public
   subnet_id           = module.network.subnet_id
   public_ip_id        = module.network.public_ip_id
-  script_url          = module.storage.script_url
   nsg_id              = module.network.nsg_id
+  script_url          = module.storage.script_url
 }
