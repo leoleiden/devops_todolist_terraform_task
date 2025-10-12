f873db65fbd032c512a955ed34ccd635a71812de
Leonid Lachmann <148564629+leoleiden@users.noreply.github.com>
2025-10-12 19:28:02 +0300

    Update outputs.tf 2

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,16 +1,29 @@
+# The subnet ID.
 output "subnet_id" {
-  description = "The ID of the Subnet"
+  description = "The ID of the subnet."
   value       = azurerm_subnet.subnet.id
 }
+
+# The Public IP Address ID. Required for NIC association.
+output "public_ip_id" {
+  description = "The ID of the Public IP Address."
+  value       = azurerm_public_ip.pip.id
+}
+
+# The Network Security Group ID. Required for NIC association.
 output "nsg_id" {
-  description = "The ID of the Network Security Group"
+  description = "The ID of the Network Security Group."
   value       = azurerm_network_security_group.nsg.id
 }
+
+# The Public IP address of the Linux VM.
 output "public_ip_address" {
-  description = "The Public IP Address of the Linux VM"
+  description = "The Public IP Address of the Linux VM."
   value       = azurerm_public_ip.pip.ip_address
 }
+
+# The FQDN of the Public IP Address.
 output "public_ip_fqdn" {
-  description = "The FQDN of the Public IP Address"
+  description = "The FQDN of the Public IP Address."
   value       = azurerm_public_ip.pip.fqdn
 }
