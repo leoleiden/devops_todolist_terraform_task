e7e740433b0f23961a61b17790230bb424e47d95
Leonid Lachmann <148564629+leoleiden@users.noreply.github.com>
2025-10-12 19:33:28 +0300

    Update outputs.tf 2

--- modules/storage/outputs.tf
+++ modules/storage/outputs.tf
@@ -1,8 +1,17 @@
+# The name of the Storage Account.
 output "storage_account_name" {
-  description = "The name of the Storage Account for application artifacts"
+  description = "The name of the Storage Account for application artifacts."
   value       = azurerm_storage_account.sa.name
 }
+
+# The name of the Storage Container.
 output "storage_container_name" {
-  description = "The name of the Storage Container for application artifacts"
+  description = "The name of the Storage Container for application artifacts."
   value       = azurerm_storage_container.sc.name
+}
+
+# The URL of the installation script in the blob storage.
+output "script_url" {
+  description = "The URL of the installation script for the VM extension."
+  value       = azurerm_storage_blob.app_script.url
 }
