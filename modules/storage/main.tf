13022488bb2d6127ac669feefb0bed01ec22e33a
Leonid Lachmann <148564629+leoleiden@users.noreply.github.com>
2025-10-12 19:30:41 +0300

    Update main.tf 2

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,3 +1,4 @@
+# Creates the storage account for application artifacts.
 resource "azurerm_storage_account" "sa" {
   name                     = var.storage_account_name
   resource_group_name      = var.resource_group_name
@@ -6,8 +7,18 @@ resource "azurerm_storage_account" "sa" {
   account_replication_type = "LRS"
 }
 
+# Creates the storage container for application artifacts.
 resource "azurerm_storage_container" "sc" {
   name                  = var.storage_container_name
   storage_account_name  = azurerm_storage_account.sa.name
   container_access_type = "blob"
+}
+
+# Uploads the application installation script to the blob container.
+resource "azurerm_storage_blob" "app_script" {
+  name                   = "install-app.sh"
+  storage_account_name   = azurerm_storage_account.sa.name
+  storage_container_name = azurerm_storage_container.sc.name
+  type                   = "Block"
+  source                 = "${path.root}/install-app.sh"
 }
