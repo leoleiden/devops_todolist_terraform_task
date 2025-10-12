61dc799b490af9098c62a6afd9d09bf88bc832fe
Leonid Lachmann <148564629+leoleiden@users.noreply.github.com>
2025-10-12 16:44:17 +0300

    Update main.tf 1

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,12 +1,52 @@
-module "compute" {
-  source                     = "./modules/compute"
-  resource_group_name        = azurerm_resource_group.rg.name
-  location                   = azurerm_resource_group.rg.location
-  vm_name                    = var.vm_name
-  vm_size                    = var.vm_size
-  ssh_key_public             = var.ssh_key_public
-  subnet_id                  = module.network.subnet_id
-  public_ip_id               = module.network.public_ip_id
-  storage_account_name       = module.storage.storage_account_name
-  script_url                 = module.storage.script_url
+resource "azurerm_network_interface" "main" {
+  name                = "${var.vm_name}-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = var.public_ip_id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                  = var.vm_name
+  resource_group_name   = var.resource_group_name
+  location              = var.location
+  size                  = var.vm_size
+  admin_username        = "adminuser"
+  network_interface_ids = [azurerm_network_interface.main.id]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = var.ssh_key_public
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_virtual_machine_extension" "main" {
+  name                 = "${var.vm_name}-install-app"
+  virtual_machine_id   = azurerm_linux_virtual_machine.main.id
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.0"
+
+  settings = <<SETTINGS
+    {
+      "script": "${var.script_url}"
+    }
+SETTINGS
 }
