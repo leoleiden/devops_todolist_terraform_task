7e63fd3acb9e07835977868dcec5e758022ace30
Leonid Lachmann <148564629+leoleiden@users.noreply.github.com>
2025-10-12 19:36:58 +0300

    Update outputs.tf 2

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -3,7 +3,20 @@ output "vm_id" {
   description = "The ID of the virtual machine."
 }
 
+# Corrected the value to get the private IP from the IP configuration.
 output "vm_private_ip" {
-  value       = azurerm_network_interface.main.private_ip_address
+  value       = azurerm_network_interface.main.ip_configuration[0].private_ip_address
   description = "The private IP address of the virtual machine."
+}
+
+# Added public IP output for easier verification.
+output "vm_public_ip" {
+  value       = azurerm_network_interface.main.ip_configuration[0].public_ip_address_id != null ? data.azurerm_public_ip.vm_pip.ip_address : ""
+  description = "The public IP address of the virtual machine."
+}
+
+data "azurerm_public_ip" "vm_pip" {
+  name                = split("/", azurerm_network_interface.main.ip_configuration[0].public_ip_address_id)[8]
+  resource_group_name = var.resource_group_name
+  depends_on          = [azurerm_linux_virtual_machine.main]
 }
